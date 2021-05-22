d489e7200b9949558851bced2361e6fadfa5714c
James Woolfenden <James.Woolfenden@gmail.com>
2021-05-22 22:00:45 +0100

    costs

--- aws_instance.examplea.tf
+++ aws_instance.examplea.tf
@@ -1,5 +1,6 @@
 
 resource "aws_instance" "examplea" {
+  # checkov:skip=CKV2_AWS_17: ADD REASON
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   monitoring    = true
