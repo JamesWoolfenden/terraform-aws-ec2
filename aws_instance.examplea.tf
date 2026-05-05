fe51d1cb96b848cec251b609fc1c04fafcd4ef13
James Woolfenden <james.woolfenden@gmail.com>
2026-05-05 15:35:46 +0100

    chore: add ghat+actionlint hooks, drop legacy workflows, skip CKV2_AWS_41, fix badge

--- aws_instance.examplea.tf
+++ aws_instance.examplea.tf
@@ -1,6 +1,7 @@
 
 resource "aws_instance" "examplea" {
   # checkov:skip=CKV2_AWS_17: ADD REASON
+  # checkov:skip=CKV2_AWS_41: instance profile not required for minimal example module
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   monitoring    = true
